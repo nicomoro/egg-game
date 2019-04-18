02a11b3a00c290b8324f8e5cac2c6a8a21936525
BLOT Martial <martial.blot@outlook.fr>
2019-04-18 17:10:47 +0200

    Nav Bar ok

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,17 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from 'src/assets/common/services/profile.service';
+import { Profile } from 'src/assets/common/models/profile.model';
+
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavBarComponent implements OnInit {
 
-  toggleNavbar = true;
+  navbarOpen = false;
+  public tableProfile = [];
+  public profil: Profile;
+  public picture: string;
+  public name: string;
+  public origin: string;
 
-  constructor() { }
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
+
+  constructor(private service: ProfileService) { }
 
   ngOnInit() {
+    this.service.getProfile().subscribe(
+      (profile: Profile) => {
+        this.profil = profile;
+        console.log(profile);
+        this.picture = profile.image;
+        this.name = profile.name;
+        this.origin = profile.origin;
+      },
+    );
   }
 
 }
--- src/assets/common/models/profile.model.ts
+++ src/assets/common/models/profile.model.ts
@@ -1,14 +1,14 @@
-export class Charactere {
+export class Profile {
+
     constructor(
+
         public id: string,
         public name: string,
         public gender: string,
         public origin: string,
         public image: string,
         public skills: [
-              string
+            string
             ]
-    ) {
-
-    }
+    ) {}
 }
