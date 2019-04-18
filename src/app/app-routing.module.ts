d58ec10b73597a0605ede6fe47f25aae8497e938
JeremyGueCo <jeremy.guerincoquereau@gmail.com>
2019-04-18 18:39:54 +0200

    MErge MAster

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { ChooseComponent } from './choose/choose.component';
+import { EggsBankComponent } from './eggs-bank/eggs-bank.component';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'choose', component: ChooseComponent },
-  { path: 'profile/:id', component: ChooseComponent }
-];
+  { path: 'profile/:id', component: ChooseComponent },
+  { path: 'eggsBank', component: EggsBankComponent },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
