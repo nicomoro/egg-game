26f86840b5590f4426d8003884be38f490d48b3d
Gordian Arragon <arragon05@gmail.com>
2019-04-18 13:57:31 +0200

    premier commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChooseComponent } from './choose/choose.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChooseComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/common/models/characteres.models.ts
@@ -0,0 +1,14 @@
+export class Charactere {
+    constructor(
+        public id: string,
+        public name: string,
+        public gender: string,
+        public origin: string,
+        public image: string,
+        public skills: [
+              string
+            ]
+    ) {
+
+    }
+}
